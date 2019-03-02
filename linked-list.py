2ef33d72c39474bf081408a8a1fdc0eba40a7194
Kimserey <kimserey.lam@gmail.com>
2019-03-02 16:18:35 +0000

    Graphs for linked list

--- linked-list.py
+++ linked-list.py
@@ -1,7 +1,53 @@
 from graphviz import Digraph
 
-g = Digraph(format='png', filename='test-output/linked-list', engine='circo')
-
+# normal graph
+g = Digraph(format='png', filename='test-output/linked-list/list')
 g.graph_attr['rankdir'] = 'LR'
+g.attr('node', shape='circle')
 g.edges(['12', '23', '34', '45', '52'])
-g.render(view=True)  
+g.render()
+
+# mu
+g = Digraph(format='png', filename='test-output/linked-list/mu')
+g.graph_attr['rankdir'] = 'LR'
+g.attr('node', shape='doublecircle', style='filled')
+g.node('2')
+g.attr('node', shape='circle', style='')
+g.edges(['12', '23', '34', '45', '52'])
+g.render()
+
+# lambda
+g = Digraph(format='png', filename='test-output/linked-list/lambda')
+g.graph_attr['rankdir'] = 'LR'
+g.attr('node', shape='circle')
+g.node('1')
+g.attr('node', style='filled')
+g.node('2')
+g.node('3')
+g.node('4')
+g.node('5')
+g.edges(['12', '23', '34', '45', '52'])
+g.render()
+
+# meeting
+g = Digraph(format='png', filename='test-output/linked-list/meeting')
+g.graph_attr['rankdir'] = 'LR'
+g.attr('node', shape='circle')
+g.node('1')
+g.node('2')
+g.node('3')
+g.node('4')
+g.attr('node', shape='doublecircle', style='filled')
+g.node('5')
+g.edges(['12', '23', '34', '45', '52'])
+g.render()
+
+# v
+g = Digraph(format='png', filename='test-output/linked-list/v')
+g.graph_attr['rankdir'] = 'LR'
+g.attr('node', shape='doublecircle', style='filled')
+g.node('1')
+g.node('2')
+g.attr('node', shape='circle', style='')
+g.edges(['12', '23', '34', '45', '52'])
+g.render()
