1ceb7abc82e86dd8fd88e4dbf465c94b5f552be1
Kimserey <kimserey.lam@gmail.com>
2019-03-02 15:35:00 +0000

    Changed engine

--- linked-list.py
+++ linked-list.py
@@ -1,6 +1,7 @@
 from graphviz import Digraph
 
-g = Digraph(format='png')
+g = Digraph(format='png', filename='test-output/linked-list', engine='circo')
+
 g.graph_attr['rankdir'] = 'LR'
 g.edges(['12', '23', '34', '45', '52'])
-g.render('test-output/linked-list', view=True)  
+g.render(view=True)  
